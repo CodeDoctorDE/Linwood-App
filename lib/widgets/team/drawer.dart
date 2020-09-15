1431b11e07f11b34b2fbdb4d1b8df87b351c5a0d
CodeDoctorDE <wusel905@gmail.com>
2020-09-15 11:17:18 +0200

    add team section

---
+++ lib/widgets/team/drawer.dart
@@ -0,0 +1,258 @@
+import 'package:flutter/material.dart';
+import 'package:linwood_app/pages.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+import '../../main.dart';
+
+class TeamDrawer extends StatefulWidget {
+  const TeamDrawer({@required this.permanentlyDisplay, Key key}) : super(key: key);
+
+  final bool permanentlyDisplay;
+  @override
+  _TeamDrawerState createState() => _TeamDrawerState();
+}
+
+class _TeamDrawerState extends State<TeamDrawer> with RouteAware {
+  String _selectedRoute;
+  AppRouteObserver _routeObserver;
+  @override
+  void initState() {
+    super.initState();
+    _routeObserver = AppRouteObserver();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _routeObserver.subscribe(this, ModalRoute.of(context));
+  }
+
+  @override
+  void dispose() {
+    _routeObserver.unsubscribe(this);
+    super.dispose();
+  }
+
+  @override
+  void didPush() {
+    _updateSelectedRoute();
+    super.didPush();
+  }
+
+  @override
+  void didPop() {
+    _updateSelectedRoute();
+    super.didPop();
+  }
+
+  bool admin = true;
+
+  @override
+  Widget build(BuildContext context) {
+    // return Drawer(
+    //     child: Row(children: [
+    //   Expanded(
+    //       child: ListView(padding: EdgeInsets.zero, children: [
+    //     DrawerHeader(
+    //       child: Column(children: [
+    //         Center(
+    //           child: new ListView(
+    //             shrinkWrap: true,
+    //             scrollDirection: Axis.horizontal,
+    //             padding: const EdgeInsets.all(20.0),
+    //             children: [
+    //               IconButton(
+    //                 icon: Icon(MdiIcons.cogs),
+    //                 onPressed: () {},
+    //               ),
+    //               IconButton(
+    //                 icon: Icon(MdiIcons.logout),
+    //                 onPressed: () {},
+    //               )
+    //             ],
+    //           ),
+    //         ),
+    //         Image.asset(
+    //           "assets/icon.png",
+    //           height: 70,
+    //         ),
+    //         SizedBox(
+    //           height: 50,
+    //         ),
+    //         Text("Evervent", style: Theme.of(context).textTheme.headline5),
+    //         Text("github.com/codedoctorde/evervent",
+    //             style: Theme.of(context).textTheme.subtitle1),
+    //       ]),
+    //     ),
+    //     ListTile(
+    //       leading: const Icon(Icons.home),
+    //       title: const Text("Home"),
+    //       onTap: () async {
+    //         await _navigateTo(context, RoutePages.home);
+    //       },
+    //       selected: _selectedRoute == RoutePages.home,
+    //     ),
+    //     ListTile(
+    //       leading: const Icon(Icons.home),
+    //       title: const Text("Settings"),
+    //       onTap: () async {
+    //         await _navigateTo(context, RoutePages.settings);
+    //       },
+    //       selected: _selectedRoute == RoutePages.settings,
+    //     ),
+    //   ]))
+    // ]));
+    return SafeArea(
+        right: false,
+        child: Drawer(
+            child: Row(children: [
+          Expanded(
+              child: ListView(padding: EdgeInsets.zero, children: [
+            FlatButton.icon(
+              color: Theme.of(context).primaryColor,
+              textTheme: ButtonTextTheme.primary,
+              icon: Icon(MdiIcons.keyboardBackspace),
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+              onPressed: () {
+                if (!widget.permanentlyDisplay) Navigator.pop(context);
+                Navigator.of(context).pop();
+              },
+              label: Text("Back"),
+            ),
+            Container(
+                padding: EdgeInsets.zero,
+                margin: EdgeInsets.zero,
+                height: 250,
+                child: DrawerHeader(
+                    child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Image.asset(
+                    "assets/icon.png",
+                    height: 70,
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Text("GuildTitle", style: Theme.of(context).textTheme.headline5),
+                  RichText(
+                    text: TextSpan(
+                      style: Theme.of(context).textTheme.bodyText2,
+                      children: <TextSpan>[
+                        TextSpan(text: 'Plan: ', style: TextStyle(fontWeight: FontWeight.bold)),
+                        TextSpan(text: 'Community'),
+                      ],
+                    ),
+                  )
+                ]))),
+            ExpansionTile(
+              initiallyExpanded: true,
+              title: Text('User'),
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        ListTile(
+                          leading: const Icon(MdiIcons.homeOutline),
+                          title: const Text("Home"),
+                          onTap: () async {
+                            await _replaceNavigateTo(context, RoutePages.guild);
+                          },
+                          selected: _selectedRoute == RoutePages.guild,
+                        ),
+                        ListTile(
+                          leading: const Icon(MdiIcons.podium),
+                          title: const Text("Karma"),
+                          onTap: () async {
+                            await _replaceNavigateTo(context, RoutePages.notification);
+                          },
+                          selected: _selectedRoute == RoutePages.notification,
+                        ),
+                        ListTile(
+                          leading: const Icon(MdiIcons.newspaperVariantMultipleOutline),
+                          title: const Text("Wiki"),
+                          onTap: () async {
+                            await _replaceNavigateTo(context, RoutePages.notification);
+                          },
+                          selected: _selectedRoute == RoutePages.notification,
+                        ),
+                        ListTile(
+                          leading: const Icon(MdiIcons.accountMultipleOutline),
+                          title: const Text("Teams"),
+                          onTap: () async {
+                            await _replaceNavigateTo(context, RoutePages.teams);
+                          },
+                          selected: _selectedRoute == RoutePages.teams,
+                        ),
+                        ListTile(
+                          leading: const Icon(MdiIcons.tune),
+                          title: const Text("Settings"),
+                          onTap: () async {
+                            await _replaceNavigateTo(context, RoutePages.guilds);
+                          },
+                          selected: _selectedRoute == RoutePages.guilds,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            IgnorePointer(
+                ignoring: !admin,
+                child: Opacity(
+                    opacity: admin ? 1 : 0.3,
+                    child: ExpansionTile(
+                        title: Text('Admin'),
+                        initiallyExpanded: admin,
+                        children: <Widget>[
+                          Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                              child: Align(
+                                  alignment: Alignment.topLeft,
+                                  child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                                    ListTile(
+                                      leading: const Icon(MdiIcons.viewDashboardOutline),
+                                      title: const Text("Dashboard"),
+                                      onTap: () async {
+                                        await _replaceNavigateTo(context, RoutePages.admin);
+                                      },
+                                      selected: _selectedRoute == RoutePages.admin,
+                                    ),
+                                    ListTile(
+                                      leading: const Icon(MdiIcons.cogs),
+                                      title: const Text("Settings"),
+                                      onTap: () async {
+                                        await _replaceNavigateTo(context, RoutePages.adminSettings);
+                                      },
+                                      selected: _selectedRoute == RoutePages.adminSettings,
+                                    )
+                                  ])))
+                        ])))
+          ])),
+          if (widget.permanentlyDisplay)
+            const VerticalDivider(
+              width: 5,
+              thickness: 0.5,
+            )
+        ])));
+  }
+
+  /// Closes the drawer if applicable (which is only when it's not been displayed permanently) and navigates to the specified route
+  /// In a mobile layout, the a modal drawer is used so we need to explicitly close it when the user selects a page to display
+  Future<void> _replaceNavigateTo(BuildContext context, String routeName) async {
+    if (!widget.permanentlyDisplay) Navigator.pop(context);
+    await Navigator.pushReplacementNamed(context, routeName);
+  }
+
+  void _updateSelectedRoute() {
+    setState(() {
+      _selectedRoute = ModalRoute.of(context).settings.name;
+    });
+  }
+}
---
+++ lib/widgets/team/scaffold.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:linwood_app/widgets/guild/drawer.dart';
+import 'package:linwood_app/widgets/team/drawer.dart';
+
+/// A responsive scaffold for our application.
+/// Displays the navigation drawer alongside the [Scaffold] if the screen/window size is large enough
+class TeamScaffold extends StatelessWidget {
+  const TeamScaffold(
+      {@required this.body,
+      @required this.pageTitle,
+      Key key,
+      this.floatingActionButton,
+      this.bottom,
+      this.actions})
+      : super(key: key);
+  final List<Widget> actions;
+
+  final Widget body;
+  final FloatingActionButton floatingActionButton;
+
+  final PreferredSizeWidget bottom;
+  final String pageTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    final bool displayMobileLayout = MediaQuery.of(context).size.width < 600;
+    return Row(
+      children: [
+        if (!displayMobileLayout)
+          const TeamDrawer(
+            permanentlyDisplay: true,
+          ),
+        Expanded(
+          child: Scaffold(
+            appBar: AppBar(
+              // when the app isn't displaying the mobile version of app, hide the menu button that is used to open the navigation drawer
+              automaticallyImplyLeading: displayMobileLayout,
+              title: Text(pageTitle),
+              actions: actions,
+              bottom: bottom,
+            ),
+            drawer: displayMobileLayout
+                ? const TeamDrawer(
+                    permanentlyDisplay: false,
+                  )
+                : null,
+            body: body,
+            floatingActionButton: floatingActionButton,
+          ),
+        )
+      ],
+    );
+  }
+}
