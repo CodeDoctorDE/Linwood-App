8f7d5e41b09e32a1d52435632c5c2de3dc2cfd35
CodeDoctorDE <wusel905@gmail.com>
2020-09-16 10:33:35 +0200

    fix error

--- lib/pages/team/home.dart
+++ lib/pages/team/home.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:linwood_app/widgets/team/scaffold.dart';
 
 class TeamPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return TeamScaffold();
+    return TeamScaffold(pageTitle: "Team", body: Center(child: Text("Welcome")));
   }
 }
